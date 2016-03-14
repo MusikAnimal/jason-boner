fe8f04d890e4096ba0fd8aead8cb70c995adf02e
Jason Boner <jasonboner@Jasons-MacBook-Pro.local>
2016-03-14 15:39:57 -0400

    Update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
+
 $(document).ready(function() {
-  // fetch data from Blogger API
   $.ajax({
     url: "https://www.googleapis.com/blogger/v3/blogs/7666529089934918798/posts",
     data: {
@@ -15,15 +15,15 @@ $(document).ready(function() {
             "OCTOBER", "NOVEMBER", "DECEMBER");
       var formattedDate = months[d.getMonth()] + ' ' + d.getDate() + ', ' + d.getFullYear();
 
-      var escapedLink = encodeURIComponent(post.title.replace(/ /g,'_'));
-      var html = "<article id='" + escapedLink + "'>" +
+      var html = "<article id='" + post.id + "'>" +
           "<header>" + 
-          "<a href='#" + escapedLink + "'>" + post.title + "</a>" + "</header>" +
-          "<time>" + formattedDate + "</time>" +
-          "</header><div class='post-body'>" + post.content + "</div>" +
+          "<a href='#" + post.id + "'>" + post.title + "</a>" +
+          "</header>" + "<time>" + formattedDate + "</time>" + "<div class='post-body'>" + post.content + "</div>" +
           "</article>";
 
       $("#blog").append(html).show();
     });
+    
+    if(location.hash) $(location.hash)[0].scrollIntoView();
   });
 });
